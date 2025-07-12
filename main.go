6cd2e24fd230166a01b3a860d90f49705d01f61b
Зубач Никита67 <zubachnikita@yandex.ru>
2025-07-12 21:02:46 +0300

    Initial commit

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package pantelapet
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
 type requestBody struct {
 	Task string `json:"Task"`
+}
+
+var task string
+
+func getTask(c echo.Context) error {
+	return c.JSON(http.StatusOK, task)
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.GET("/task", getTask)
+
+	e.Start("localhost:8080")
 }
